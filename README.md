1b77c8673c10cfc0ece439b0ebf8be853390fa4b
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-04-08 16:33:45 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Pipes
-
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  names = '';
+  date = '';
+  amount = 0;
+  miles = 0;
+
+  onMilesChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    let value = target.value;
+    this.miles = parseFloat(value);
+  }
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.names = target.value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.date = target.value;
+  }
+
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    let value = target.value;
+
+    this.amount = parseFloat(value);
+  }
 }
